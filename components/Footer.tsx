8367dd027b7eac2c2861cc2154cebae66a063ee5
MontMor4 <victorperial@hotmail.com>
2022-08-11 15:14:51 -0300

    Leroy

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,13 +2,13 @@ import Image from "next/image";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="footer mt-5 flex h-24 w-full items-center justify-center bg-neutral text-neutral-content">
+    <footer className="footer mt-7 flex h-24 w-full items-center justify-center text-neutral-content">
       <div className="grid-flow-col items-center">
         <Image
-          src={"https://i.ibb.co/MPZVFyj/menu-Logo-Horizontal.png"}
+          src={"https://i.ibb.co/znT0mVK/Logo-Compet-White.png"}
           alt="Logo do COMPET"
-          width={175}
-          height={70}
+          width={120}
+          height={120}
         />
       </div>
       <div className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
@@ -18,6 +18,7 @@ const Footer: React.FC = () => {
             width="24"
             height="24"
             viewBox="0 0 50 50"
+            color="white"
             className="fill-current"
           >
             <path d="M41,4H9C6.24,4,4,6.24,4,9v32c0,2.76,2.24,5,5,5h32c2.76,0,5-2.24,5-5V9C46,6.24,43.76,4,41,4z M17,20v19h-6V20H17z M11,14.47c0-1.4,1.2-2.47,3-2.47s2.93,1.07,3,2.47c0,1.4-1.12,2.53-3,2.53C12.2,17,11,15.87,11,14.47z M39,39h-6c0,0,0-9.26,0-10 c0-2-1-4-3.5-4.04h-0.08C27,24.96,26,27.02,26,29c0,0.91,0,10,0,10h-6V20h6v2.56c0,0,1.93-2.56,5.81-2.56 c3.97,0,7.19,2.73,7.19,8.26V39z" />
@@ -29,6 +30,7 @@ const Footer: React.FC = () => {
             width="24"
             height="24"
             viewBox="0 0 24 24"
+            color="white"
             className="fill-current"
           >
             <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
@@ -40,6 +42,7 @@ const Footer: React.FC = () => {
             width="24"
             height="24"
             viewBox="0 0 24 24"
+            color="white"
             className="fill-current"
           >
             <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
