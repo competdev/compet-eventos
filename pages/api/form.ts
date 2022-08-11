386ad02cced099b60895f2bd67a7b8445133dd8a
Fhilipfhc <fhilipfhc@gmail.com>
2022-08-11 00:13:02 -0300

    teste qrcode no email

--- pages/api/form.ts
+++ pages/api/form.ts
@@ -2,6 +2,7 @@ import { PrismaClient } from "@prisma/client";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { FormInput } from "../../types";
 import nodemailer from "nodemailer";
+import QRcode from "qrcode";
 
 export default async function handler(
   req: NextApiRequest,
@@ -29,12 +30,42 @@ export default async function handler(
         pass: "fgnhatcvvrdkvelq",
       },
     });
+let code = 0
     
+    // Require the package 
+const QRCode = require('qrcode') 
+  
+// Creating the data 
+let data = { 
+    email: "formInput.email" 
+} 
+  
+// Converting the data into String format 
+let stringdata = JSON.stringify(data) 
+  
+// Print the QR code to terminal 
+QRCode.toString(stringdata,{type:'terminal'}, 
+                    function (err, QRcode) { 
+  
+    if(err) return console.log("error occurred") 
+  
+    // Printing the generated code 
+    console.log(QRcode) 
+}) 
+    
+// Converting the data into base64 
+QRCode.toDataURL(stringdata, function (err, code) { 
+    if(err) return console.log("error occurred") 
+  
+    // Printing the code 
+    console.log(code) 
+})
+
     let mailOptions = {
       from: "compet.eventos@gmail.com",
       to: formInput.email,
       subject: `Confirmaçao Evento COMPET`,
-      html: `QRCode de confirmação com id do user`, //aqui vcs enviam o login do usuário.
+      html: code, //aqui vcs enviam o login do usuário.
     };
     
     transporter.sendMail(mailOptions, function (err, info) {
@@ -49,5 +80,9 @@ export default async function handler(
   } catch (error) {
     res.status(500).json({ error: "Erro na criação do usuário" });
   }
+
+
+
+
 }
 
